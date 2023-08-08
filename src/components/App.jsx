00fb37ad777c70c50fc47db55d8507c8cc835b83
pavelchuktaras <121377064+pavelchuktaras@users.noreply.github.com>
2023-08-08 19:39:09 +0300

    comit my hw :(

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,18 +5,25 @@ import { nanoid } from 'nanoid';
 import { ContactForm } from "./ContactForm/ContactForm";
 import { ContactList } from "./ContactList/ContactList";
 
-
-
 export class App extends Component {
   state = {
-  contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
+  contacts: [],
   filter: ''
-}
+  }
+  
+ componentDidMount() {
+    const contactsFromLocalStorage = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contactsFromLocalStorage);
+    if (!parsedContacts) return;
+    this.setState({ contacts: parsedContacts });
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+			 localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+		}
+  }
+
   addContact = (name, number) => {
   const newContact = {
     id: nanoid(),
@@ -33,8 +40,6 @@ export class App extends Component {
   }));
 };
 
-
-   
   handleChange = (e) => {
 
      const { name, value } = e.target;
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -30,7 +30,7 @@ export class ContactForm extends Component {
                     type="text"
                     value={name}
                   name="name"
-                  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                  pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                   title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan" required
                 />
                 </label>
@@ -40,7 +40,7 @@ export class ContactForm extends Component {
                   type="tel"
                   value={number}
                   name="number"
-                  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                  pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
                   title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                   required
                 />              
